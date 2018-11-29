cf73e717ed2bbfb616c005e275de4ce0e0af7b22
Develog <marcleger.9999@gmail.com>
2018-11-29 17:38:30 +0100

    #7

---
+++ Projet/src/projet/Command.java
@@ -0,0 +1,8 @@
+package projet;
+
+public class Command {
+	
+	public Command() {
+		
+	}	
+}
---
+++ Projet/src/projet/ItemTicket.java
@@ -0,0 +1,17 @@
+package projet;
+
+public class ItemTicket extends Item {
+
+	private final String name;
+	private final int quantity;
+	
+	public ItemTicket(String name, int quantity) {
+		super(name, quantity);
+		this.name = name;
+		this.quantity = quantity;
+	}
+
+	public String getDescription() {
+		return this.name + " : " + this.quantity;
+	}
+}
